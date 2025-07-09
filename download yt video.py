8b3ce4f685652ddc0c5434028f401f08ecb412c4
Aidan180804 <aidanswaine13@gmail.com>
2025-07-09 13:57:05 +0100

    Update download yt video.py

--- download yt video.py
+++ download yt video.py
@@ -8,14 +8,16 @@ file_name = input('enter output file name:').strip('""')
 output_folder = r'C:\Users\aidan\OneDrive - The University of Nottingham\Documents\auto highlight project'
 output_path = os.path.join(output_folder, file_name + '.%(ext)s')
 
+def download_youtube_as_mp4(url)
+
 ydl_opts = {
     'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',  # best quality
     'outtmpl': output_path,  # full output path
     'merge_output_format': 'mp4',
 }
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+       ydl.download([url])
     
 # extract mp3
 
@@ -37,4 +39,5 @@ def download_youtube_as_mp3(url, output_folder):
     with yt_dlp.YoutubeDL(ydl_opts_mp3) as ydl:
         ydl.download([url])
 
+download_youtube_as_mp4(url, output_path)
 download_youtube_as_mp3(url, output_path)
