d3f1b0044b7ddbdcaa990605b3688d808b89f490
Aidan180804 <aidanswaine13@gmail.com>
2025-07-09 15:15:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from download yt video.py import ask_for_url
+from download_yt_video import ask_for_url
+from download_yt_video import download_youtube_as_mp4
+from download_yt_video import download_youtube_as_mp3
+
+url, output_path = ask_for_url()
+download_youtube_as_mp4(url, output_path)
+download_youtube_as_mp3(url, output_path)
