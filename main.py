946fbfa2c2f35d57efc0e5338650608c78cecf4f
Aidan180804 <aidanswaine13@gmail.com>
2025-07-09 15:36:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ from download_yt_video import download_youtube_as_mp3
 url, output_path = ask_for_url()
 download_youtube_as_mp4(url, output_path)
 download_youtube_as_mp3(url, output_path)
+
+
+
+
+
+file_path = input("Enter path to mp3 file: ").strip('""')
+print(f"File path entered: '{file_path}'")
+audio = load_audio(file_path)
+intervals = get_loud_sections(audio)
+intervals.rename(columns = {0 : 'start', 1 : 'end'}, inplace=True)
+if not intervals:
+  print("No loud sections found.")
+     
+else:
+  print("complete.")
