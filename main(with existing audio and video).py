c825c6540939204a7d55d43a2a7f0fba29020dc4
Aidan180804 <aidanswaine13@gmail.com>
2025-07-13 19:55:02 +0100

    Update and rename main(with existing audio and video),py to main(with existing audio and video).py

--- main(with existing audio and video).py
+++ main(with existing audio and video).py
@@ -1,3 +1,13 @@
+# download yt video
+from download_yt_video import ask_for_url
+from download_yt_video import download_youtube_as_mp4
+from download_yt_video import download_youtube_as_mp3
+
+url, output_path, output_folder = ask_for_url()
+download_youtube_as_mp4(url, output_path)
+download_youtube_as_mp3(url, output_path)
+
+
 # loud sections from mp3
 from loud_sections_from_mp3 import load_audio
 from loud_sections_from_mp3 import get_loud_sections
@@ -14,9 +24,16 @@ intervals.rename(columns = {0 : 'start', 1 : 'end'}, inplace=True)
 # edit mp4
 from edit_mp4  import extract_video_clips
 from edit_mp4 import merge_and_rename_highlights
+import sys
 
 input_video = input('enter path to mp4  file')
-output_dir = r'C:\Users\aidan\OneDrive - The University of Nottingham\Documents\auto highlight project\audio files'
-merged_dir = r'C:\Users\aidan\OneDrive - The University of Nottingham\Documents\auto highlight project'
+output_dir = input('enter different folder directory:').strip('""')
+merged_dir = output_folder
+if output_dir == merged_dir:
+   print("Output directory and merged directory cannot be the same. Exiting program.")
+   sys.exit()
+else:
+    print("Directories are valid. Continuing with processing...")
+
 extract_video_clips(input_video, output_dir, intervals)
 merge_and_rename_highlights(output_dir, merged_dir)
